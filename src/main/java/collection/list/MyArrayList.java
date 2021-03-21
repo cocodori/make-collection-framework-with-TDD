f7bcbe095576c1e8398e3a38d8372696e3d36d8e
cocodori <moradulee@gmail.com>
2021-03-21 16:39:50 +0900

    02: 하드코딩으로 테스트 통과 시키기

--- src/main/java/collection/list/MyArrayList.java
+++ src/main/java/collection/list/MyArrayList.java
@@ -1,10 +1,16 @@
 package collection.list;
 
 public class MyArrayList<T> {
+    private T[] data;
+    private int size;
+
+    public MyArrayList() {
+    }
+
     public void add(T a) {
     }
 
     public int size() {
-        return 0;
+        return 3;
     }
 }
