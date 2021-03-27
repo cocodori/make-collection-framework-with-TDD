4440f492e0d33dd40d4cde832f86f8ea677a10b5
cocodori <moradulee@gmail.com>
2021-03-27 10:44:57 +0900

    [LinkedList] add(E e) 테스트 통과 시키기)

--- src/main/java/collection/list/DoublyLinkedList.java
+++ src/main/java/collection/list/DoublyLinkedList.java
@@ -1,7 +1,24 @@
 package collection.list;
 
+import java.util.LinkedList;
+
 public class DoublyLinkedList<E> {
-    public boolean add(String world) {
-        return false;
+    Node node;
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node(Node<E> prev, E item, Node<E> next) {
+            this.prev = prev;
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    public boolean add(E item) {
+        node = new Node(null, item, null);
+        return true;
     }
 }
